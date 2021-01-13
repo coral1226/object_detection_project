9b58234215a51a5acdfa6f2caa02d19c55e74fac
subidubob <77367757+subidubob@users.noreply.github.com>
2021-01-13 10:55:25 +0900

    확장 클래스 생성

--- vision/transforms/transforms.py
+++ vision/transforms/transforms.py
@@ -330,7 +330,7 @@ class RandomSampleCrop(object):
 
                 return current_image, current_boxes, current_labels
 
-
+# 확장팩
 class Expand(object):
     def __init__(self, mean):
         self.mean = mean
