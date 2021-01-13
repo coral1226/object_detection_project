c366669bb722c2e1b5d700111569a6644d2a92da
SeungWon Yang <swingfly@naver.com>
2021-01-13 10:57:15 +0900

    Update move_file.py
    Add description

--- utils/move_file.py
+++ utils/move_file.py
@@ -1,14 +1,10 @@
 import os
 import shutil
 
-# 경로 변경 
-# target = "/home/bong20/data/iitp/train_data/track4/train_data"
-# result = "/home/bong20/data/iitp/train_data/track4/train"
-
 target = "/home/bong20/data/iitp/track4/train_data"
 result = "/home/bong20/data/iitp/track4/train"
 
-# 파일 포멧 형식 추가 
+# Only image files move to target folder
 
 for dirName, subdirList, fileList in os.walk(target):
     if os.path.basename(dirName) != "image":
