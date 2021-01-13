7db747ef07606f19664468b7fbf3fc318843809d
subidubob <77367757+subidubob@users.noreply.github.com>
2021-01-13 10:53:37 +0900

    이미지 색상 변경 클래스 생성

--- vision/transforms/transforms.py
+++ vision/transforms/transforms.py
@@ -166,7 +166,26 @@ class RandomLightingNoise(object):
             image = shuffle(image)
         return image, boxes, labels
 
+# 이미지 색상 변경 클래스 생성
+# class ConvertColor(object):
+#     def __init__(self, current, transform):
+#         self.transform = transform
+#         self.current = current
 
+#     def __call__(self, image, boxes=None, labels=None):
+#         if self.current == 'BGR' and self.transform == 'HSV':
+#             image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#         elif self.current == 'RGB' and self.transform == 'HSV':
+#             image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+#         elif self.current == 'BGR' and self.transform == 'RGB':
+#             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         elif self.current == 'HSV' and self.transform == 'BGR':
+#             image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+#         elif self.current == 'HSV' and self.transform == "RGB":
+#             image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+#         else:
+#             raise NotImplementedError
+#         return image, boxes, labels
 class ConvertColor(object):
     def __init__(self, current, transform):
         self.transform = transform
