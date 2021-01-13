2405729932bd1cfec543adc85784592737bb4392
sybaek <sybaek.nexys@gmail.com>
2021-01-13 10:06:32 +0900

    학습용 커밋

---
+++ test_ssd.py
@@ -0,0 +1,36 @@
+from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
+from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
+from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite, create_mobilenetv1_ssd_lite_predictor
+from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor
+from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
+from vision.ssd.mobilenetv3_ssd_lite import create_mobilenetv3_large_ssd_lite, create_mobilenetv3_small_ssd_lite
+from vision.utils.misc import Timer
+import cv2
+import sys
+
+
+net_type = sys.argv[1]
+class_names = ["person"]
+
+if net_type == 'vgg16-ssd':
+    net = create_vgg_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd':
+    net = create_mobilenetv1_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd-lite':
+    net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb2-ssd-lite':
+    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb3-large-ssd-lite':
+    net = create_mobilenetv3_large_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb3-small-ssd-lite':
+    net = create_mobilenetv3_small_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'sq-ssd-lite':
+    net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
+else:
+    print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
+    sys.exit(1)
+
+print(sum(p.numel() for p in net.parameters() if p.requires_grad))
+# for param_tensor in net.state_dict():
+#     print(param_tensor, "\t", net.state_dict()[param_tensor].dtype)
+exit()
