c154c2262a88ed4ab316d655de99fe3a6e91ff13
SeungWon Yang <swingfly@naver.com>
2021-01-13 11:02:59 +0900

    Update run_ssd_example.py

--- run_ssd_example.py
+++ run_ssd_example.py
@@ -59,7 +59,7 @@ boxes, labels, probs = predictor.predict(image, 10, 0.4)
 for i in range(boxes.size(0)):
     box = boxes[i, :]
     cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
-    #label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
+
     label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
     cv2.putText(orig_image, label,
                 (box[0] + 20, box[1] + 40),
